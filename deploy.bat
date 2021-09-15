e13e3c8a1c9fd7538f674757b7d48b99f66e0537
Kashimura <kashimura@qq.com>
2021-09-15 17:09:24 +0800

    update

---
+++ deploy.bat
@@ -0,0 +1,15 @@
+
+rem 生成静态文件
+npm run build
+
+rem 进入生成的文件夹
+cd docs/.vuepress/dist
+
+rem deploy to github pages
+echo 'www.kashimura.cc' > CNAME
+git init
+git add -A
+git commit -m "deploy"
+
+rem 推送到github gh-pages分支
+git push -f git@github.com:KashimuraSama/vuepress-blog.git master:gh-pages
