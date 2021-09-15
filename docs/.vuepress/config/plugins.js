1a844f21a0ed5461d7ea67c0f43f5b39c39d85a0
Kashimura <kashimura@qq.com>
2021-09-15 14:53:02 +0800

    更改名称

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -85,9 +85,9 @@ module.exports = [
       options: {
         clientID: '07c69d2120799dd9a8c8',
         clientSecret: '65b8233ec89c3e949aca0f06aa87723188757bad',
-        repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'Kashimura', // GitHub仓库所有者
-        admin: ['Kashimura'], // 对仓库有写权限的人
+        repo: 'kashimura-blog', // GitHub 仓库
+        owner: 'KashimuraSama', // GitHub仓库所有者
+        admin: ['KashimuraSama'], // 对仓库有写权限的人
         // distractionFreeMode: true,
         pagerDirection: 'last', // 'first'正序 | 'last'倒序
         id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
@@ -106,5 +106,29 @@ module.exports = [
         return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
       },
     },
-  ]
+  ],
+  [
+    'vuepress-plugin-helper-live2d', {
+      live2d: {
+        // 是否启用(关闭请设置为false)(default: true)
+        enable: true,
+        // 模型名称(default: hibiki)>>>取值请参考：
+        // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+        model: 'hibiki',
+        display: {
+          position: "right", // 显示位置：left/right(default: 'right')
+          width: 135, // 模型的长度(default: 135)
+          height: 300, // 模型的高度(default: 300)
+          hOffset: 65, //  水平偏移(default: 65)
+          vOffset: 0, //  垂直偏移(default: 0)
+        },
+        mobile: {
+          show: false // 是否在移动设备上显示(default: false)
+        },
+        react: {
+          opacity: 0.8 // 模型透明度(default: 0.8)
+        }
+      }
+    }
+  ],
 ]
